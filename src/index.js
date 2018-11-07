d9cc5bedad4f4b397bb089ff4ac9559f085c5a71
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-11-07 17:47:28 +0100

    chore: Allow injecting custom webpack and jest configs into CRA

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-import RematchStore from './services/Rematch'
+import RematchStore from '~services/Rematch'
 
 import App from './App'
 
