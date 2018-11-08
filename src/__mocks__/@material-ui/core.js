d14c3fa5e5ef04125c9357a20c8d7c4eb8db9874
jakubdrozdek <jakub.drozdek@acaisoft.com>
2018-11-08 14:50:47 +0100

    fix: Material UI's withStyles mock

--- src/__mocks__/@material-ui/core.js
+++ src/__mocks__/@material-ui/core.js
@@ -3,7 +3,7 @@ import { makeGetProxy } from '~utils/proxies'
 // Custom mocks
 const definedMocks = {
   createMuiTheme: jest.fn(),
-  withStyles: jest.fn(() => jest.fn()),
+  withStyles: jest.fn(() => jest.fn(component => component)),
 }
 
 // Mock any not defined components automatically: Component -> ComponentMock
