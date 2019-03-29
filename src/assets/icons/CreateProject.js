c0b773f4ffb9bc3ec5ff953ccf8b4acd8ff2357e
jakubdrozdek <jakub.drozdek@acaisoft.com>
2019-03-29 17:47:19 +0100

    fix: Convert SVG attributes to camelCase

--- src/assets/icons/CreateProject.js
+++ src/assets/icons/CreateProject.js
@@ -12,7 +12,7 @@ function CreateProject(props) {
       <path
         d="M81 69H53.7819H5C2.79086 69 1 67.2091 1 65V5C1 2.79086 2.79086 1 5 1H102.564C104.773 1 106.564 2.79086 106.564 5V35V42"
         stroke="#A7A7C5"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <rect x="3" y="58" width="78" height="2" fill="#A7A7C5" />
       <ellipse cx="9.87928" cy="9" rx="1.97315" ry="2" fill="#9696D1" />
@@ -21,37 +21,37 @@ function CreateProject(props) {
       <path
         d="M14.2039 24V46.1311H40.463M14.2039 24L11.8523 27.6885M14.2039 24L16.5554 27.6885M40.463 46.1311L36.9357 43.2623M40.463 46.1311L36.9357 49"
         stroke="#A7A7C5"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
       <path
         d="M18.7583 40L21.9153 34.5L28.2294 37.6429L34.5435 29"
         stroke="#A7A7C5"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
       <path
         d="M49.8354 19H84.3657"
         stroke="#A7A7C5"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
       <path
         d="M49.3423 25H83.8725"
         stroke="#A7A7C5"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
       <path
         d="M49.3423 32H83.8725"
         stroke="#A7A7C5"
-        stroke-width="2"
-        stroke-linecap="round"
+        strokeWidth="2"
+        strokeLinecap="round"
       />
       <path
         d="M40.6772 78L42.3328 69H62.2438L64.7916 78H40.6772Z"
         stroke="#A7A7C5"
-        stroke-width="2"
+        strokeWidth="2"
       />
       <rect x="32.5704" y="77" width="41.4362" height="2" rx="1" fill="#A7A7C5" />
       <rect x="90.7786" y="58" width="13.8121" height="2" rx="1" fill="#32D4C0" />
