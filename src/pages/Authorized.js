7ac92ba07937ddd695244e8c824169ccbe79db74
jakubdrozdek <jakub.drozdek@acaisoft.com>
2019-04-01 09:37:34 +0200

    chore: Change main route to projects

--- src/pages/Authorized.js
+++ src/pages/Authorized.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react'
 
 import { Redirect, Route, Switch } from 'react-router-dom'
 
-import DashboardPage from './Dashboard'
 import ProjectsPage from './Projects'
 import TestConfigurationsPage from './TestConfigurations'
 import TestExecutionsPage from './TestExecutions'
@@ -12,12 +11,11 @@ export class Authorized extends Component {
   render() {
     return (
       <Switch>
-        <Route path="/dashboard" exact component={DashboardPage} />
         <Route path="/projects" component={ProjectsPage} />
         <Route path="/test-configurations" component={TestConfigurationsPage} />
         <Route path="/test-runs" component={TestExecutionsPage} />
         <Route path="/test-sources" component={TestSourcesPage} />
-        <Redirect from="*" to="/dashboard" />
+        <Redirect from="*" to="/projects" />
       </Switch>
     )
   }
