e06a07593e660bd3eeb4504450c0fb0965098b13
Bartosz Rak <bartosz.rak@acaisoft.com>
2019-04-19 09:46:08 +0000

    fix: Use query for a single configuration [ACBT-209]

--- src/containers/TestConfiguration/TestConfiguration.container.js
+++ src/containers/TestConfiguration/TestConfiguration.container.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 import { Query } from 'react-apollo'
-import { GET_CONFIGS_QUERY } from '~services/GraphQL/Queries'
+import { GET_CONFIG_QUERY } from './graphql'
 
 export class TestConfiguration extends Component {
   static propTypes = {
@@ -12,12 +12,12 @@ export class TestConfiguration extends Component {
   render() {
     const { children, configurationId } = this.props
     return (
-      <Query query={GET_CONFIGS_QUERY} variables={{ configurationId, limit: 1 }}>
+      <Query query={GET_CONFIG_QUERY} variables={{ configurationId }}>
         {({ loading, error, data }) =>
           children({
             loading,
             error,
-            data: data && data.configuration && data.configuration[0],
+            data: data && data.configuration_by_pk,
           })
         }
       </Query>
---
+++ src/containers/TestConfiguration/graphql.js
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag'
+
+export const GET_CONFIG_QUERY = gql`
+  query getTestConfiguration($configurationId: uuid) {
+    configuration_by_pk(id: $configurationId) {
+      id
+      name
+      configuration_type {
+        id
+        name
+      }
+      configuration_parameters {
+        id
+        value
+        parameter_slug
+      }
+      executions {
+        id
+        start
+      }
+      test_source {
+        id
+        source_type
+        repository {
+          id
+          name
+          url
+        }
+        test_creator {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+export default GET_CONFIG_QUERY
