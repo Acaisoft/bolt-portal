0226c4f6873231d130f3e0d31def74c733a336ab
Jakub Drozdek <jakub.drozdek@acaisoft.com>
2019-04-27 19:58:08 +0200

    refactor: Rewrite Pagination as stateless component

--- src/containers/Pagination/Pagination.js
+++ src/containers/Pagination/Pagination.js
@@ -1,69 +1,49 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 import { TablePagination } from '@material-ui/core'
 
-export class Pagination extends Component {
-  static propTypes = {
-    onChange: PropTypes.func,
-    offset: PropTypes.number,
-    rowsPerPage: PropTypes.number,
-    totalCount: PropTypes.number,
-  }
+const offsetToPage = (offset, rowsPerPage) => parseInt(offset / rowsPerPage, 10)
+const pageToOffset = (page, rowsPerPage) => page * rowsPerPage
 
-  static defaultProps = {
-    onChange: () => {},
-  }
+function Pagination({
+  offset = 0,
+  onChange = () => {},
+  rowsPerPage = 10,
+  rowsPerPageOptions = [5, 10, 25],
+  totalCount,
+}) {
+  const page = offsetToPage(offset, rowsPerPage)
 
-  constructor(props) {
-    super(props)
-
-    const rowsPerPage = props.rowsPerPage || 10
-    const page = props.offset ? props.offset / rowsPerPage : 0
-
-    this.state = {
-      page,
-      rowsPerPage,
-    }
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { page, rowsPerPage } = this.state
-
-    if (prevState.page !== page || prevState.rowsPerPage !== rowsPerPage) {
-      const calculated = {
-        offset: page * rowsPerPage,
-        page,
-        rowsPerPage,
-      }
-      this.props.onChange(calculated)
-    }
-  }
-
-  handleChangePage = (e, page) => {
-    this.setState({ page })
-  }
-
-  handleChangeRowsPerPage = e => {
-    this.setState({ page: 0, rowsPerPage: e.target.value })
-  }
-
-  render() {
-    const { totalCount } = this.props
-    const { page, rowsPerPage } = this.state
-
-    return (
-      <TablePagination
-        component="div"
-        rowsPerPageOptions={[5, 10, 25]}
-        rowsPerPage={rowsPerPage}
-        count={totalCount}
-        page={page}
-        onChangePage={this.handleChangePage}
-        onChangeRowsPerPage={this.handleChangeRowsPerPage}
-      />
-    )
-  }
+  return (
+    <TablePagination
+      component="div"
+      rowsPerPageOptions={rowsPerPageOptions}
+      rowsPerPage={rowsPerPage}
+      count={totalCount}
+      page={page}
+      onChangePage={(e, newPage) => {
+        onChange({
+          page: newPage,
+          offset: pageToOffset(newPage, rowsPerPage),
+          rowsPerPage,
+        })
+      }}
+      onChangeRowsPerPage={e => {
+        onChange({
+          page: 0,
+          offset: 0,
+          rowsPerPage: e.target.value,
+        })
+      }}
+    />
+  )
+}
+Pagination.propTypes = {
+  onChange: PropTypes.func,
+  offset: PropTypes.number,
+  rowsPerPage: PropTypes.number,
+  totalCount: PropTypes.number,
 }
 
 export default Pagination
