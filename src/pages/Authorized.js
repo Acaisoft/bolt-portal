5b768af4e25e364c6f77a6770cb0c5c3a3a2d0c7
jakubdrozdek <jakub.drozdek@acaisoft.com>
2019-05-06 16:26:50 +0200

    chore: Remove unused routes from Authorized

--- src/pages/Authorized.js
+++ src/pages/Authorized.js
@@ -3,18 +3,12 @@ import React, { Component } from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
 import ProjectsPage from './Projects'
-import TestConfigurationsPage from './TestConfigurations'
-import TestExecutionsPage from './TestExecutions'
-import TestSourcesPage from './TestSources'
 
 export class Authorized extends Component {
   render() {
     return (
       <Switch>
         <Route path="/projects" component={ProjectsPage} />
-        <Route path="/test-configurations" component={TestConfigurationsPage} />
-        <Route path="/test-runs" component={TestExecutionsPage} />
-        <Route path="/test-sources" component={TestSourcesPage} />
         <Redirect from="*" to="/projects" />
       </Switch>
     )
