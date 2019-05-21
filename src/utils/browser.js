b620d7215978268750725dbdee908cdb9cac7d73
jakubdrozdek <jakub.drozdek@acaisoft.com>
2019-05-21 11:34:01 +0200

    ref: Extract copying logic from copyValueToClipboard

--- src/utils/browser.js
+++ src/utils/browser.js
@@ -2,7 +2,13 @@ export const copyValueToClipboard = value => {
   const el = document.createElement('textarea')
   el.value = value
   document.body.appendChild(el)
+
+  copyValueFromInput(el)
+
+  document.body.removeChild(el)
+}
+
+export const copyValueFromInput = el => {
   el.select()
   document.execCommand('copy')
-  document.body.removeChild(el)
 }
