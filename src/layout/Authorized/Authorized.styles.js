1463a1bbbfd38d119f70e60137cf65546789e9b9
Tomasz Szczęsny <tomasz.szczesny@acaisoft.com>
2019-06-05 14:55:18 +0000

    style: Add responsive margin for site content

--- src/layout/Authorized/Authorized.styles.js
+++ src/layout/Authorized/Authorized.styles.js
@@ -1,11 +1,12 @@
 import { makeStyles } from '@material-ui/core'
 
-export default makeStyles(({ mixins, spacing }) => ({
+export default makeStyles(({ breakpoints, spacing }) => ({
   root: {},
   content: {
-    ...mixins.gutters(),
     paddingTop: spacing(3),
     paddingBottom: spacing(3),
+    [breakpoints.up("md")]: { paddingLeft: spacing(9), paddingRight: spacing(9) },
+    [breakpoints.down("md")]: { paddingLeft: spacing(3), paddingRight: spacing(3) }
   },
   nav: {
     display: 'flex',
