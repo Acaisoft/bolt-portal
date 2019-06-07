04dedebdc7a774709d6a0363caffd31c31f3bbbb
jakubdrozdek <jakub.drozdek@acaisoft.com>
2019-06-07 14:28:39 +0200

    feat: Allow setting precision in formatThousands

--- src/utils/numbers.js
+++ src/utils/numbers.js
@@ -2,6 +2,6 @@ import accounting from 'accounting'
 
 export const formatNumber = accounting.formatNumber
 
-export const formatThousands = n => formatNumber(n, 0, ' ')
+export const formatThousands = (n, precision = 0) => formatNumber(n, precision, ' ')
 export const formatPercent = (n, precision = 2) =>
   `${formatNumber(n * 100.0, precision)}%`
