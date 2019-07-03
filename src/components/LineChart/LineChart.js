8db652bb1829eb32857cf038cde12bc13e5604ef
Tomasz Bajda <tomasz.bajda@acaisoft.com>
2019-07-03 13:34:27 +0000

    feat: change charts lib [ACBT-360]

---
+++ src/components/LineChart/LineChart.js
@@ -0,0 +1,174 @@
+import React, { useCallback } from 'react'
+import moment from 'moment'
+
+import filesize from 'filesize'
+import { formatThousands, formatPercent } from '~utils/numbers'
+
+import ReactEcharts from 'echarts-for-react'
+import { withStyles } from '@material-ui/styles'
+
+const formatters = {
+  bytes: {
+    axis: value => filesize(value, { round: 2, exponent: 3 }),
+    tooltip: value => filesize(value, { round: 3 }),
+  },
+  number: {
+    axis: value => formatThousands(value),
+    tooltip: value => formatThousands(value),
+  },
+  percent: {
+    axis: value => formatPercent(value, 0),
+    tooltip: value => formatPercent(value, 2),
+  },
+}
+
+const formatTimestamp = timestamp => moment(timestamp).format('HH:mm:ss')
+
+function calculateTooltipPosition(mouse, params, dom, rect, size) {
+  const [offsetX, offsetY] = [10, 10]
+
+  const [mouseX, mouseY] = mouse
+  const [viewX, viewY] = size.viewSize
+  const { offsetWidth: width, offsetHeight: height } = dom
+  const fitsX = mouseX + width < viewX
+  const fitsY = mouseY + height < viewY
+
+  return {
+    top: fitsY ? mouseY + offsetY : mouseY - height - offsetY,
+    left: fitsX ? mouseX + offsetX : mouseX - width - offsetX,
+  }
+}
+
+export function LineChart({ data, config, groupNames, theme }) {
+  const { color, gridLine, font, tooltip } = theme.palette.chart
+
+  const lineColors = [
+    color.line.primary,
+    color.line.success,
+    color.line.error,
+    'green',
+    'red',
+    'blue',
+    'violet',
+    'orange',
+    'pink',
+  ]
+
+  const formatTooltip = useCallback(
+    params => {
+      const series = Array.isArray(params) ? params : [params]
+
+      return `
+<div style="padding: 16px;">
+  <div>${series[0].name}</div>
+  <ul style="margin: 8px; padding: 0; list-style: none">
+  ${series
+    .map((serie, index) => {
+      const formatedValue = formatters[config.y_format].tooltip(serie.value)
+
+      return `
+  <li style="margin: 0px; display: flex; flex-wrap: wrap; flex-grow: 1; flex-basis: 30%; align-items: center">
+    <div style="margin-right: 4px;">${serie.marker}</div>
+    <div style="font-weight: bold;">${serie.seriesName}</div>
+    <div style="flex-basis: 100%; margin-left: 17px">${formatedValue}</div>
+  </li>
+    `
+    })
+    .join('\n')}
+  </ul>
+</div>
+  `
+    },
+    [config]
+  )
+
+  const options = React.useMemo(() => {
+    return {
+      legend: {
+        show: false,
+      },
+      color: lineColors,
+      grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        top: '3%',
+        containLabel: true,
+      },
+      tooltip: {
+        trigger: 'axis',
+        backgroundColor: tooltip.fill,
+        textStyle: {
+          color: font.color,
+          fontFamily: font.fontFamily,
+        },
+        formatter: formatTooltip,
+        position: calculateTooltipPosition,
+      },
+      xAxis: {
+        boundaryGap: false,
+        type: 'category',
+        data: data.map(d => formatTimestamp(d.timestamp)),
+        splitLine: {
+          lineStyle: {
+            type: 'dashed',
+            color: gridLine.color,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: gridLine.color,
+          },
+        },
+      },
+      yAxis: {
+        min: config.y_format === 'percent' ? 0 : null,
+        max: config.y_format === 'percent' ? 1 : null,
+        type: 'value',
+        splitLine: {
+          lineStyle: {
+            type: 'dashed',
+            color: gridLine.color,
+          },
+        },
+        axisLine: {
+          lineStyle: {
+            color: gridLine.color,
+          },
+        },
+        axisLabel: {
+          formatter: formatters[config.y_format].axis,
+        },
+      },
+      textStyle: {
+        color: font.color,
+        fontFamily: font.fontFamily,
+      },
+      series: groupNames.map((groupName, groupIndex) => {
+        return {
+          name: groupName,
+          type: 'line',
+          showSymbol: false,
+          animation: false,
+          data: data.map((datum, datumIndex) => {
+            return datum.groups[groupName]
+          }),
+        }
+      }),
+    }
+  }, [data, config, font, gridLine, lineColors, tooltip, groupNames, formatTooltip])
+
+  return (
+    <ReactEcharts
+      option={options}
+      notMerge={true}
+      opts={{
+        renderer: 'canvas',
+        width: 'auto',
+        height: 'auto',
+      }}
+    />
+  )
+}
+
+export default withStyles({}, { withTheme: true })(LineChart)
--- src/pages/TestExecutions/Monitoring/components/MonitoringLineChart/MonitoringLineChart.js
+++ src/pages/TestExecutions/Monitoring/components/MonitoringLineChart/MonitoringLineChart.js
@@ -1,61 +1,17 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
-import moment from 'moment'
-import {
-  LineChart,
-  Line,
-  CartesianGrid,
-  ResponsiveContainer,
-  Tooltip,
-  XAxis,
-  YAxis,
-} from 'recharts'
 
-import { withStyles } from '@material-ui/core'
-
-import filesize from 'filesize'
-import { formatThousands, formatPercent } from '~utils/numbers'
-import { SectionHeader, ChartTooltip } from '~components'
+import { SectionHeader } from '~components'
 
 import ChartFilter from '../ChartFilter'
+import LineChart from '~components/LineChart'
 
-const formatters = {
-  bytes: {
-    axis: value => filesize(value, { round: 2, exponent: 3 }),
-    tooltip: value => filesize(value, { round: 3 }),
-  },
-  number: {
-    axis: value => formatThousands(value),
-    tooltip: value => formatThousands(value),
-  },
-  percent: {
-    axis: value => formatPercent(value, 0),
-    tooltip: value => formatPercent(value, 2),
-  },
-}
-
-const formatTimestamp = timestamp => moment(timestamp).format('HH:mm:ss')
-
-export function MonitoringLineChart({ config, data, groupNames, theme }) {
-  const { color, gridLine, font } = theme.palette.chart
-
-  // TODO: Assign better colors
-  const lineColors = [
-    color.line.primary,
-    color.line.success,
-    color.line.error,
-    'green',
-    'red',
-    'blue',
-    'violet',
-    'orange',
-    'pink',
-  ]
-
+export function MonitoringLineChart({ config, data, groupNames }) {
   const [selected, setSelected] = useState([...groupNames])
 
   return (
     <React.Fragment>
+      {' '}
       <SectionHeader title={config.title} size="small" marginBottom>
         <ChartFilter
           groupNames={groupNames}
@@ -63,63 +19,7 @@ export function MonitoringLineChart({ config, data, groupNames, theme }) {
           selected={selected}
         />
       </SectionHeader>
-
-      <ResponsiveContainer width="100%" height={300}>
-        <LineChart
-          style={{ ...font }}
-          data={data}
-          margin={{
-            top: 10,
-            bottom: 30,
-            right: 40,
-            left: 20,
-          }}
-        >
-          <CartesianGrid strokeDasharray={gridLine.dash} stroke={gridLine.color} />
-          <XAxis
-            axisLine={{ strokeDasharray: gridLine.dash, stroke: gridLine.color }}
-            type={config.x_format}
-            dataKey={config.x_data_key}
-            name={config.x_label}
-            tickFormatter={formatTimestamp}
-            tick={{ ...font, fontSize: 12 }}
-            domain={['dataMin', 'dataMax']}
-          />
-          <YAxis
-            axisLine={{ strokeDasharray: gridLine.dash }}
-            tick={{ ...font, fontSize: 12, width: 80 }}
-            tickFormatter={formatters[config.y_format].axis}
-            domain={config.y_format === 'percent' ? [0, 1] : ['dataMin', 'dataMax']}
-            interval="preserveStartEnd"
-          />
-
-          <Tooltip
-            content={<ChartTooltip />}
-            isAnimationActive={false}
-            labelFormatter={formatTimestamp}
-            formatter={formatters[config.y_format].tooltip}
-            wrapperStyle={{ zIndex: 1 }}
-          />
-
-          {selected.map((groupName, index) => {
-            const lineColor = lineColors[index % lineColors.length]
-
-            return (
-              <Line
-                key={groupName}
-                type="linear"
-                strokeWidth={2}
-                stroke={lineColor}
-                fill={lineColor}
-                dataKey={tick => tick.groups[groupName]}
-                name={groupName}
-                dot={false}
-                isAnimationActive={false}
-              />
-            )
-          })}
-        </LineChart>
-      </ResponsiveContainer>
+      <LineChart data={data} config={config} groupNames={selected} />
     </React.Fragment>
   )
 }
@@ -127,7 +27,6 @@ MonitoringLineChart.propTypes = {
   config: PropTypes.object.isRequired,
   data: PropTypes.array.isRequired,
   groupNames: PropTypes.array.isRequired,
-  theme: PropTypes.object.isRequired,
 }
 
-export default withStyles({}, { withTheme: true })(MonitoringLineChart)
+export default MonitoringLineChart
