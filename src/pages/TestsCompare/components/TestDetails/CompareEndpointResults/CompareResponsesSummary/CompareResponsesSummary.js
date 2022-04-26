c48512c1b992dfbee3def986858cb6d013627d1a
Jakub.Szostko <jakub.szostko@acaisoft.com>
2022-04-26 12:45:57 +0000

    Merged in hotfix/endpoint-summary-formatting (pull request #260)
    fix(CompareResponsesSummary): add numbers formatting
    
    * fix(CompareResponsesSummary): add numbers formatting

--- src/pages/TestsCompare/components/TestDetails/CompareEndpointResults/CompareResponsesSummary/CompareResponsesSummary.js
+++ src/pages/TestsCompare/components/TestDetails/CompareEndpointResults/CompareResponsesSummary/CompareResponsesSummary.js
@@ -1,26 +1,49 @@
 import React from 'react'
 import { Grid } from '@material-ui/core'
+import { formatThousands, formatPercent } from 'utils/numbers'
 import { useEndpointSummary } from 'hooks'
 import { LabeledValue, NoWrap, SectionHeader } from 'components'
 
 function CompareResponsesSummary({ data }) {
-  const summary = useEndpointSummary(data)
+  const {
+    successes,
+    requests,
+    failures,
+    requestsPerSecond,
+    minResponseTime,
+    averageResponseTime,
+    maxResponseTime,
+  } = useEndpointSummary(data)
 
   return (
     <>
       <SectionHeader title="Summary" size="small" marginBottom />
       <Grid container spacing={5} alignItems="flex-start">
         <Grid item xs={6} sm={4}>
-          <LabeledValue label="Total requests" value={summary.requests || 0} />
+          <LabeledValue label="Total requests" value={formatThousands(requests)} />
         </Grid>
         <Grid item xs={6} sm={4}>
-          <LabeledValue label="Success" value={summary.successes || 0} />
+          <LabeledValue
+            label="Success"
+            value={
+              <NoWrap>
+                {formatPercent(successes / requests)} ({formatThousands(successes)})
+              </NoWrap>
+            }
+          />
         </Grid>
         <Grid item xs={6} sm={4}>
-          <LabeledValue label="Fail" value={summary.failures || 0} />
+          <LabeledValue
+            label="Fail"
+            value={
+              <NoWrap>
+                {formatPercent(failures / requests)} ({formatThousands(failures)})
+              </NoWrap>
+            }
+          />
         </Grid>
         <Grid item xs={6} sm={4}>
-          <LabeledValue label="Req/s" value={summary.requests || 0} />
+          <LabeledValue label="Req/s" value={formatThousands(requestsPerSecond)} />
         </Grid>
         <Grid item xs={6} sm={4}>
           <LabeledValue
@@ -31,7 +54,13 @@ function CompareResponsesSummary({ data }) {
                 <NoWrap>Min. / Avg. / Max.</NoWrap>
               </div>
             }
-            value={`${summary.minResponseTime} / ${summary.averageResponseTime} / ${summary.maxResponseTime}`}
+            value={
+              <NoWrap>
+                {formatThousands(minResponseTime)} /{' '}
+                {formatThousands(averageResponseTime)} /{' '}
+                {formatThousands(maxResponseTime)}
+              </NoWrap>
+            }
           />
         </Grid>
       </Grid>
